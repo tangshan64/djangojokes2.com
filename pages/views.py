1ca05291ca046df18cbea055d9786d2f1f1b376e
Jeff Zuckerberg <tangshan64@gmail.com>
2025-01-11 20:13:54 -0500

    Further commits

---
+++ pages/views.py
@@ -0,0 +1,8 @@
+from django2.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = 'pages/home.html'
+
+
+class AboutUsView(TemplateView):
+    template_name = 'pages/about_us.html'
